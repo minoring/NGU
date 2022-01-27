ecf795c69e1da670cd02517ac42f63177c80ffcd
First Last <minho@lim-b.usc.edu>
2022-01-27 06:15:09 -0800

    Modify minimum sequence to start replay

--- ngu/models/model_hypr.py
+++ ngu/models/model_hypr.py
@@ -58,8 +58,7 @@ model_hypr = dict(
     # replay_capacity=5000000 // 120,  # 5M / SEQUENCE_LENGTH
     # minimum_sequences_to_start_replay=6250,
     replay_capacity=5000000 // 120 // 2,
-    # minimum_sequences_to_start_replay=1024,
-    minimum_sequences_to_start_replay=64,
+    minimum_sequences_to_start_replay=2048,
     # Last N frames of the sampled sequences to trian the action prediction networkand RND.trinsic novelty.
     num_frame_intrinsic_train=5,
 )
